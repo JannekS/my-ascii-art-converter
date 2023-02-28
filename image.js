91cc215ae142b6160785b94706c57e56c846ac36
Jannek <janneks@tutanota.com>
2023-03-01 00:01:41 +0100

    Implemented function to convert image to ascii art and log on console.

--- image.js
+++ image.js
@@ -1,23 +1,55 @@
+const { raw } = require('express');
 const sharp = require('sharp');
-const imageSrc = './temp/zebra-test-small.jpg'; 
+const imageSrc = './temp/zebra-test.jpg'; 
 
-// async function getMetadata(src) {
-//     const metadata = await sharp(src).metadata();
+async function createAsciiArt(src, maxWidth, inverted) {
+    // create validators for arguments
+    let widthOut = maxWidth;
+    const metadata = await sharp(src).metadata();
+    if (metadata.width < maxWidth) {
+        widthOut = metadata.width;
+    }
 
-//     console.log(metadata);
+    const { data, info } = await sharp(src)
+        .resize({width: widthOut})
+        .raw()
+        .toBuffer({ resolveWithObject: true });
+    
+    const rawArray = new Uint8ClampedArray(data.buffer);
+    const { width, height } = info;
+    const pixelArr = [];
+    const pixelMatrix = [];
 
-// };
+    let asciiString = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+    if (inverted) {
+        let invertedSring = asciiString.split("").reverse("").join("");
+        asciiString = invertedSring
+    }
+    let imageString = "";
 
-function convertToGreyscale(src) {
-    sharp(src).greyscale().toFile('./temp/test.jpg').then(info => {
-        console.log(info)
-            ;    });
+    for (i = 0; i < rawArray.length; i += 3) {
+        let brightness = (0.21 * rawArray[i] + 0.72 * rawArray[i+1] + 0.07 * rawArray[i+2])
+        pixelArr.push(brightness);
+    }
+
+    for (i = 0; i < height; i++) {
+        pixelMatrix[i] = [];
+        for (j = 0; j < width; j++) {
+            pixelMatrix[i][j] = pixelArr[i * width + j];
+        }
+    }
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            const mapValue = Math.floor(pixelMatrix[i][j] / 4);
+            imageString += asciiString.charAt(mapValue);
+            imageString += asciiString.charAt(mapValue);
+        }
+        imageString += "\n";
+    }
+
+    console.log(imageString);
 }
 
-convertToGreyscale(imageSrc);
-
-// getMetadata(imageSrc);
-
-
-
+createAsciiArt(imageSrc, 400);
 
